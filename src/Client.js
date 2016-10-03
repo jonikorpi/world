03d4a2bfcb14e96ead9274d27eac3ee426175659
Joni Korpi <me@jonikorpi.com>
2016-10-03 15:26:26 +0300

    Anonymous auth on load

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import firebase from "firebase";
 
-class App extends Component {
+class Client extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      uid: null,
+      anonymous: null,
+      connected: false,
+      haveConnectedOnce: true,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        this.setState({
+          uid: user.uid,
+          anonymous: user.isAnonymous,
+        });
+      }
+      else {
+        this.setState({
+          uid: null,
+          anonymous: null,
+        });
+
+        firebase.auth().signInAnonymously().catch(function(error) {
+          console.log(error);
+        });
+      }
+    }.bind(this));
+
+    firebase.database().ref(".info/connected").on("value", function(online) {
+      if (online.val() === true) {
+        this.setState({
+          connected: true,
+          haveConnectedOnce: true,
+        });
+      }
+      else {
+        this.setState({connected: false});
+      }
+    }.bind(this));
+  }
+
   render() {
     return (
       <div className="client">
@@ -10,4 +55,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Client;
