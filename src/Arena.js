fef337d3d7e712576d548c024825de43863429da
Joni Korpi <me@jonikorpi.com>
2016-10-04 18:36:11 +0300

    Fetches game from Firebase

--- src/Arena.js
+++ src/Arena.js
@@ -1,33 +1,67 @@
 import React, { Component } from "react";
-import shallowCompare from "react-addons-shallow-compare";
+import firebase from "firebase";
+import reactMixin from "react-mixin";
+import ReactFire from "reactfire";
 
 import Game from "./Game";
 
 export default class Arena extends Component {
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this.state = {
-  //   };
-  // }
+  constructor(props) {
+    super(props);
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState);
+    this.state = {
+      game: undefined,
+    }
+
+    this.bindFirebase = this.bindFirebase.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.params.arenaID) {
+      this.bindFirebase(this.props.params.arenaID);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.params.arenaID !== this.props.params.arenaID) {
+      if (this.firebaseRefs.firebase) {
+        this.unbind("firebase");
+      }
+      if (nextProps.uid) {
+        this.bindFirebase(nextProps.params.arenaID);
+      }
+    }
+  }
+
+  bindFirebase(arenaID) {
+    this.bindAsObject(
+      firebase.database().ref(`games/${arenaID}`),
+      "game",
+      function(error) {
+        console.log("Firebase subscription cancelled:")
+        console.log(error);
+        this.setState({firebase: undefined})
+      }.bind(this)
+    );
   }
 
   render() {
+    const arenaID = this.props.params.arenaID;
+    const game = this.state.game;
+
     return (
       <div>
         <h2>Arena</h2>
         <pre>{this.props.params.arenaID}</pre>
 
-        <p>Owner game starting controls</p>
-
-        <Game
-          arenaID={this.props.params.arenaID}
-          playerID={this.props.playerID}
-        />
+        {
+          game && game[arenaID]
+          ? <Game arenaID={arenaID} game={game[arenaID]} playerID={this.props.playerID}/>
+          : <p>Owner game starting controls / profile visitor controls</p>
+        }
       </div>
     );
   }
 }
+
+reactMixin(Arena.prototype, ReactFire);
