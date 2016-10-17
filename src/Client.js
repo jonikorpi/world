7541c0caad7aa7df0020a8b4800499034b0f4780
Joni Korpi <me@jonikorpi.com>
2016-10-17 23:11:18 +0300

    Moves default light

--- src/Client.js
+++ src/Client.js
@@ -152,9 +152,9 @@ export default class Client extends Component {
               intensity: 1.5,
             }}
             position={[
-              -0.75,
+              0,
               1,
-              0.5,
+              -0.5,
             ]}
           />
 
