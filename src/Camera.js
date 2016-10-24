69f57f1c17ce4b8db5d7dbf9480431b16abab27a
Joni Korpi <me@jonikorpi.com>
2016-10-24 16:07:36 +0300

    Adds placeholder for keyboard nav

--- src/Camera.js
+++ src/Camera.js
@@ -2,50 +2,67 @@ import React, { PureComponent } from "react";
 import { Entity } from "aframe-react";
 
 export default class Camera extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    // TODO: add fenced keyboard nav
+    this.state = {
+      x: 0,
+      z: 0,
+    };
+  }
+
   render() {
+    const position = this.props.inVR ? [0,0,0] : [this.state.x, 0, this.state.z];
+
     return (
       <Entity
-        id="camera"
-        camera={{
-          far: this.props.far,
-          near: this.props.near,
-          fov: this.props.inVR ? 80 : 90,
-          userHeight: 1.75,
-        }}
-        universal-controls={{
-          movementEnabled: this.props.inVR === false,
-          movementSpeed:        20,
-          movementEasing:       25,
-          movementAcceleration: 20,
-          // rotationSensitivity:  0.05,
-          // fly: true,
-        }}
+        id="locomotion"
+        position={position}
       >
         <Entity
-          id="cursor"
-          raycaster={{
-            far: 1100,
-            near: 0.1,
-            interval: 80,
-            objects: ".interactable",
-            recursive: true,
+          id="camera"
+          camera={{
+            far: this.props.far,
+            near: this.props.near,
+            fov: this.props.inVR ? 80 : 90,
+            userHeight: 1.75,
           }}
-          cursor={{
-            fuse: false,
+          universal-controls={{
+            movementEnabled: this.props.inVR === false,
+            movementSpeed:        20,
+            movementEasing:       25,
+            movementAcceleration: 20,
+            // rotationSensitivity:  0.05,
+            // fly: true,
           }}
-          geometry={{
-            primitive: "ring",
-            radiusInner: 0.0056,
-            radiusOuter: 0.0091,
-            segmentsTheta: 16,
-            segmentsPhi: 1,
-          }}
-          position={[0, 0, -0.5]}
-          material={{
-            shader: "flat",
-            color: "white",
-          }}
-        />
+        >
+          <Entity
+            id="cursor"
+            raycaster={{
+              far: 1100,
+              near: 0.1,
+              interval: 80,
+              objects: ".interactable",
+              recursive: true,
+            }}
+            cursor={{
+              fuse: false,
+            }}
+            geometry={{
+              primitive: "ring",
+              radiusInner: 0.0056,
+              radiusOuter: 0.0091,
+              segmentsTheta: 16,
+              segmentsPhi: 1,
+            }}
+            position={[0, 0, -0.5]}
+            material={{
+              shader: "flat",
+              color: "white",
+            }}
+          />
+        </Entity>
       </Entity>
     );
   }
