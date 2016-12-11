04cc90f9a2bf49fc5bfc1db3069ea66cd26f4251
Joni Korpi <me@jonikorpi.com>
2016-12-11 22:16:53 +0200

    Comments out leftover if

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import React, { PureComponent } from "react";
 import Scene from "../components/Scene";
 import Head from "../components/Head";
 
-if (process.env.NODE_ENV !== "production" && typeof window !== "undefined") {
+// if (process.env.NODE_ENV !== "production" && typeof window !== "undefined") {
   // window.ReactPerf = require("react-addons-perf");
   // window.ReactPerf.start();
   // firebase.database.enableLogging(true);
-}
+// }
 
 export default class Play extends PureComponent {
   render() {
