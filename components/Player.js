6b45520d12958b062f33841e5b4ab1f72cdfef36
Joni Korpi <me@jonikorpi.com>
2017-02-23 18:37:35 +0200

    Removes playerRipple to stop Mobile Safari from crashing

--- components/Player.js
+++ components/Player.js
@@ -10,20 +10,6 @@ export default class Player extends PureComponent {
     return (
       <div id={`player-${this.props[".key"]}`} className="player">
         <style jsx>{`
-          .playerRipple {
-            position: absolute;
-            width:  calc(var(--worldScale) * 0.5vmin);
-            height: calc(var(--worldScale) * 1vmin);
-            border-radius: 38% 38% 23% 23%;
-            will-change: transform;
-            transform:
-              translate(-50%, -50%)
-              rotate(var(--playerAngle))
-              scale( calc( 1 + var(--playerVelocity) * 5 ) )
-            ;
-            background: ${colors.bright};
-          }
-
           .playerModel {
             position: absolute;
             width:  calc(var(--worldScale) * 0.5vmin);
@@ -44,8 +30,6 @@ export default class Player extends PureComponent {
             line-height: 2.25;
           }
         `}</style>
-
-        <div className="playerRipple"/>
 
         <div
           className="playerModel"
