eace26cd09268955618bca3f72cd3958bc39f6ba
Joni Korpi <me@jonikorpi.com>
2017-03-02 13:46:29 +0200

    Adds a dummy item example

--- helpers/item.js
+++ helpers/item.js
@@ -1,3 +1,18 @@
 let item = {};
 
+item.collection = [
+  {
+    name: "Cheap Blinking Item",
+    type: "defense",
+    maxDurability: 1,
+    cost: 1,
+    onUse: () => {
+      // blink
+    },
+    onHit: undefined,
+    onEquip: undefined,
+    onBreak: undefined,
+  },
+];
+
 export default item;
